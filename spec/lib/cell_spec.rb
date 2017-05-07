4ad6d26a59a3f411cc96f4d5cdbcd899c395b142
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-05-06 19:11:58 -0700

    Initial

---
+++ spec/lib/cell_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+require "game"
+require "cell"
+
+describe Cell do
+  it "Any live cell with fewer than two live neighbours dies, as if caused by underpopulation." do
+
+  end
+
+  it "Any live cell with two or three live neighbours lives on to the next generation." do
+
+  end
+
+  it "Any live cell with more than three live neighbours dies, as if by overpopulation." do
+
+  end
+
+  it "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." do
+    
+  end
+end
