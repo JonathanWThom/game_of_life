1ee574166a4b5264fe9ed550f747e62f9e272569
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-03-31 11:08:32 -0700

    Fixes cell_spec failures, if not the logic of it all

--- spec/lib/cell_spec.rb
+++ spec/lib/cell_spec.rb
@@ -3,6 +3,7 @@ require "game"
 require "cell"
 
 describe Cell do
+  let(:grid) { Grid.new(3, 3) }
 
   describe "#neighbors" do
     it "will return true if one cell is a neighbor of another, including diagonals" do
@@ -23,27 +24,27 @@ describe Cell do
 
   describe "#less_than_two_neighbors" do
     it "Any live cell with fewer than two live neighbours dies, as if caused by underpopulation." do
-      game = Game.new(3, 3, [0, 4])
+      game = Game.new(3, 3, [0, 4], grid)
       game.next_turn
       expect(game.cells[4].living).to eq(false)
     end
   end
 
   it "Any live cell with two live neighbours lives on to the next generation." do
-    game = Game.new(3, 3, [0, 1, 4])
+    game = Game.new(3, 3, [0, 1, 4], grid)
     game.next_turn
     expect(game.cells[4].living).to eq(true)
   end
 
   it "Any live cell with three live neighbours lives on to the next generation." do
-    game = Game.new(3, 3, [0, 1, 2, 4])
+    game = Game.new(3, 3, [0, 1, 2, 4], grid)
     game.next_turn
     expect(game.cells[4].living).to eq(true)
   end
 
   describe "#more_than_three_neighbors" do
     it "Any live cell with more than three live neighbours dies, as if by overpopulation." do
-      game = Game.new(3, 3, [0, 1, 2, 3, 4])
+      game = Game.new(3, 3, [0, 1, 2, 3, 4], grid)
       game.next_turn
       expect(game.cells[4].living).to eq(false)
     end
@@ -51,7 +52,7 @@ describe Cell do
 
   describe "#exactly_three_neighbors" do
     it "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." do
-      game = Game.new(3, 3, [1, 3, 5])
+      game = Game.new(3, 3, [1, 3, 5], grid)
       game.next_turn
       expect(game.cells[4].living).to eq(true)
     end
